3d72ab2f6e95d3d4a0e880da7b94fba3f0cb8736
Jero <loygorri@gmail.com>
2021-11-25 18:27:24 +0100

    Some BigDL

--- modules/bigDL/src/main/scala/Main.scala
+++ modules/bigDL/src/main/scala/Main.scala
@@ -1,5 +1,9 @@
-import org.apache.spark.sql.SparkSession
 import com.intel.analytics.bigdl.utils.Engine
+// Hay que usar NumericFloat o Numeric Double para elegir el tipo de los tensores.
+import com.intel.analytics.bigdl.tensor.Tensor
+import com.intel.analytics.bigdl.numeric.NumericFloat
+import com.intel.analytics.bigdl.utils.T
+
 
 
 object Main {
@@ -9,16 +13,24 @@ object Main {
   }
 
   def holaBigDL()={
+
     println("Hola BigDL")
-    val nodeNumber = 1
-    val coreNumber = 4
-    val mult = 64
-    val batchSize = nodeNumber * coreNumber * mult
-    Engine.init(nodeNumber, coreNumber, false)
-    println("Done")
+    Engine.init
+    val tensor = Tensor(2, 3)
+    println(s"Tensor vacio de 2x3:\n ${tensor}")
+
+    val a = Tensor(T(
+      T(1f, 2f, 3f),
+      T(4f, 5f, 6f)))
+    println(s"Tensor a:\n${a}")
+
+    println(s"+1 a todo el tensor:\n ${a+1}")
+
+    println(s"Tensor traspuesto:\n ${a.t}")
   }
 
   def holaDesdeSpark() = {
+    import org.apache.spark.sql.SparkSession
     val spark = SparkSession
       .builder()
       .appName("Spark Pi")
@@ -26,7 +38,7 @@ object Main {
       .getOrCreate()
 
     println("Hola BigDL")
-    
+
     val nodeNumber = 1
     val coreNumber = 4
     val mult = 64
